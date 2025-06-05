224a7976a8d3a4e6a071e46aa367e0b7e56860c8
mathiase4 <mathiaseriksson98@hotmail.com>
2025-06-05 14:20:16 +0200

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,14 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+
+    runGame("addition");
+
 });
 
 /**
@@ -22,11 +26,20 @@ document.addEventListener("DOMContentLoaded", function () {
  * and after the user's answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
 let num1 = Math.floor(Math.random() * 25) + 1;
 let num2 = Math.floor(Math.random() * 25) + 1;
 
+if (gameType === "addition") {
+    displayAdditionQuestion(num1, num2);
+
+} else {
+    alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}. Aborting!`;
 }
+}
+
+
 
 
 
@@ -46,7 +59,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+document.getElementById("operand1").textContent = operand1;
+document.getElementById("operand2").textContent = operand2;
+document.getElementById("operator").textContent = "+";
 
 }
 
