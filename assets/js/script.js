6562a1af659567292e07b09858e5e52b44eb0c58
mathiase4 <mathiaseriksson98@hotmail.com>
2025-06-06 12:06:44 +0200

    safe git

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,9 +30,12 @@ function runGame(gameType) {
 let num1 = Math.floor(Math.random() * 25) + 1;
 let num2 = Math.floor(Math.random() * 25) + 1;
 
+
 if (gameType === "addition") {
     displayAdditionQuestion(num1, num2);
 
+} else if (gameType === "multiplay") {
+displayMultiplyQuestion(num1, num2);
 } else {
     alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. Aborting!`;
@@ -71,7 +74,9 @@ function calculateCorrectAnswer() {
 
  if (operator === "+") {
     return [operand1 + operand2, "addition"];
-
+ } else if  (operator === "x") {
+return [operand1 * operand2, "multiply"];
+ 
  } else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator  ${operator}. Aborting!`;
@@ -107,6 +112,8 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
-
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
 }
