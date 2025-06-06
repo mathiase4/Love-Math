aec5a53b022713feea2027cde5e3351da440bf0d
mathiase4 <mathiaseriksson98@hotmail.com>
2025-06-06 12:53:10 +0200

    Add multiplicatin game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,11 @@
-//Wait for the DOM to finish loading before running the game
-//Get the button elements and add event listeners to them
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
-
     for (let button of buttons) {
-        button.addEventListener("click", function () {
+        button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
                 checkAnswer();
             } else {
@@ -16,7 +15,6 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-
     runGame("addition");
 
 });
@@ -25,95 +23,102 @@ document.addEventListener("DOMContentLoaded", function () {
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-
 function runGame(gameType) {
-let num1 = Math.floor(Math.random() * 25) + 1;
-let num2 = Math.floor(Math.random() * 25) + 1;
 
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
-if (gameType === "addition") {
-    displayAdditionQuestion(num1, num2);
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
-} else if (gameType === "multiplay") {
-displayMultiplyQuestion(num1, num2);
-} else {
-    alert(`unknown game type: ${gameType}`);
-        throw `unknown game type: ${gameType}. Aborting!`;
 }
-}
-
 
 /**
- * checks the answer against the first element in
- * the returned calculatecorrectanswer array
+ * Checks the answer against the first element in
+ * the returned calculateCorrectAnswer array
  */
-
 function checkAnswer() {
- let userAnswer = parseInt(document.getElementById("answer-box").value);
- let calculatedAnswer = calculateCorrectAnswer();
- let isCorrect = userAnswer === calculatedAnswer[0];
- if (isCorrect) {
- alert("Hey! You got it right!");
- incrementScore();
- } else {
-    alert(`Awwww...arguments.callee.apply. you answered ${userAnswer}, the correct answer was ${calculatedAnswer [0]}!`);
-    incrementWrongAnswer();
- }
- runGame(calculatedAnswer [1]);
+
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+        incrementScore();
+    } else {
+        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
+    }
+
+    runGame(calculatedAnswer[1]);
+
 }
 
 /**
  * Gets the operands (the numbers) and the operator (plus, minus etc)
  * directly from the dom, and returns the correct answer.
  */
-
 function calculateCorrectAnswer() {
- let operand1 = parseInt(document.getElementById("operand1").innerText);
- let operand2 = parseInt(document.getElementById("operand2").innerText);
- let operator = document.getElementById("operator").innerText;
 
- if (operator === "+") {
-    return [operand1 + operand2, "addition"];
- } else if  (operator === "x") {
-return [operand1 * operand2, "multiply"];
- 
- } else {
-    alert(`Unimplemented operator ${operator}`);
-    throw `Unimplemented operator  ${operator}. Aborting!`;
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
 
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 
- }
 }
+
 /**
  * Gets the current score from the DOM and increments it by 1
  */
 function incrementScore() {
-let oldScore = paraInt(document.getElementById("score").innerText);
-document.getElementById("score").innerText = ++oldScore;
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
+
 /**
  * Gets the current tally of incorrect answers from the DOM and increments it by 1
  */
 function incrementWrongAnswer() {
-    let oldScore = paraInt(document.getElementById("incorrect").innerText);
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
     document.getElementById("incorrect").innerText = ++oldScore;
+    
 }
 
 function displayAdditionQuestion(operand1, operand2) {
-document.getElementById("operand1").textContent = operand1;
-document.getElementById("operand2").textContent = operand2;
-document.getElementById("operator").textContent = "+";
 
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+    
 }
 
 function displaySubtractQuestion() {
 
-
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
-    document.getElementById("operand1").textContent = operand1;
-    document.getElementById("operand2").textContent = operand2;
-    document.getElementById("operator").textContent = "x";
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
+
 }
